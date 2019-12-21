3c0ae80fc960e402d89513f5d4ba7640c030148c
mohammed <mdrame113@gmail.com>
2019-12-20 23:11:37 -0800

    UICircularProgressrING Test

---
+++ wordPlay/wordPlay/Controller/HomeViewController.swift
@@ -0,0 +1,42 @@
+//
+//  HomeViewController.swift
+//  
+//
+//  Created by Mohammed Drame on 12/15/19.
+//  Copyright © 2019 Mo Drame. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    
+    // Outlets
+    @IBOutlet weak var heartButtonOutlet: UIButton!
+    
+    
+    // variables
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    // Actions
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+ 
+        
+    }
+    
+    
+
+
+    
+    
+
+}
