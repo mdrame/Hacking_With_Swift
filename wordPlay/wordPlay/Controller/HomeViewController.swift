7adae66bb124f7bad79740921a448134340a6d6f
mohammed <mdrame113@gmail.com>
2019-12-20 23:50:49 -0800

    vc established

--- wordPlay/wordPlay/Controller/HomeViewController.swift
+++ wordPlay/wordPlay/Controller/HomeViewController.swift
@@ -20,6 +20,8 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        
 
         // Do any additional setup after loading the view.
     }
